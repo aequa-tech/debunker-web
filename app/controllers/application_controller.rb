fedf6a5ea6e21f80b46fe6e20d04bd0f60001c05
Daniele Giorgis <daniele.giorgis@welaika.com>
2024-04-15 14:38:21 +0200

    Improvement for application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   layout 'application'
 
   before_action :authenticate_user!
+  before_action :set_user
+
+  private
+
+  def set_user
+    @user = current_user
+  end
 end
--- app/controllers/users/dashboard_controller.rb
+++ app/controllers/users/dashboard_controller.rb
@@ -1,8 +1,5 @@
 module Users
   class DashboardController < ApplicationController
-    def index
-      # The `current_user` method is provided by Devise and returns the currently signed-in user
-      @user = current_user
-    end
+    def index; end
   end
 end
