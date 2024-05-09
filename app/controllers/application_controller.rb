844ba4ec2c0ee30e36232c34dee6cd3fc5d152b7
Daniele Giorgis <daniele.giorgis@welaika.com>
2024-05-09 09:59:09 +0200

    Better login specs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,10 @@ class ApplicationController < ActionController::Base
 
   private
 
+  def after_sign_in_path_for(_resource)
+    user_root_path
+  end
+
   def set_user
     @user = current_user
   end
--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -7,42 +7,102 @@ RSpec.describe 'User login', type: :feature do
     context 'when user not confirmed' do
       let(:user) { create(:user) }
 
-      before do
-        sign_in user
-        visit root_path
+      context 'with logged in user' do
+        before do
+          sign_in user
+          visit root_path
+        end
+
+        it 'redirect to login path' do
+          expect(current_path).to eq new_user_session_path
+        end
+
+        it 'show flash message saying that must confirm account' do
+          expect(page).to have_css('.notices .notice__text', text: I18n.t('devise.failure.unconfirmed'))
+        end
       end
 
-      it 'redirect to login path' do
-        expect(current_path).to eq new_user_session_path
-      end
+      context 'whitout logged in user' do
+        before do
+          visit root_path
+        end
 
-      it 'show flash message saying that must confirm account' do
-        expect(page).to have_css('.notices .notice__text', text: I18n.t('devise.failure.unconfirmed'))
+        context 'when user insert email and password' do
+          before do
+            within('.navbar') do
+              click_on I18n.t('devise.links.sign_in')
+            end
+
+            within('form') do
+              fill_in 'user_email', with: user.email
+              fill_in 'user_password', with: user.password
+              click_on I18n.t('devise.links.sign_in')
+            end
+          end
+
+          it 'show flash message saying that must confirm account' do
+            expect(page).to have_css('.notices .notice__text', text: I18n.t('devise.failure.unconfirmed'))
+          end
+        end
       end
     end
 
     context 'when user is confirmed' do
       let(:user) { create(:user, :confirmed) }
 
-      before do
-        sign_in user
-        visit root_path
-      end
-
-      it 'show root_path' do
-        expect(current_path).to eq root_path
-      end
-
-      it 'show dashboard link in header' do
-        expect(page).to have_css('.navbar a', text: 'Dashboard')
-      end
-
-      it 'can logout' do
-        visit user_root_path
-        within('aside.navigator') do
-          click_on I18n.t('navigation.account.sign_out')
+      context 'with logged in user' do
+        before do
+          sign_in user
+          visit root_path
         end
-        expect(current_path).to eq new_user_session_path
+
+        it 'show root_path' do
+          expect(current_path).to eq root_path
+        end
+
+        it 'show dashboard link in header' do
+          expect(page).to have_css('.navbar a', text: I18n.t('navigation.dashboard'))
+        end
+
+        it 'can logout' do
+          visit user_root_path
+          within('aside.navigator') do
+            click_on I18n.t('navigation.account.sign_out')
+          end
+          expect(current_path).to eq new_user_session_path
+        end
+      end
+
+      context 'whitout logged in user' do
+        before do
+          visit root_path
+        end
+
+        context 'when user insert email and password' do
+          before do
+            within('.navbar') do
+              click_on I18n.t('devise.links.sign_in')
+            end
+
+            within('form') do
+              fill_in 'user_email', with: user.email
+              fill_in 'user_password', with: user.password
+              click_on I18n.t('devise.links.sign_in')
+            end
+          end
+
+          it 'show root_path' do
+            expect(current_path).to eq user_root_path
+          end
+
+          it 'show notice message' do
+            expect(page).to have_css('.notices .notice__text', text: I18n.t('devise.sessions.signed_in'))
+          end
+
+          it 'show dashboard link in header' do
+            expect(page).to have_content(user.email)
+          end
+        end
       end
     end
   end
