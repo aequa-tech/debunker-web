169f0844791497d87bb158179c67d78a9c8f5d31
Alessandro Fazzi <alessandro.fazzi@welaika.com>
2024-08-06 13:44:35 +0200

    sentry: update config
    - fix typo in env variable name
    - disable traces and profiles by setting sample rate to 0

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,5 +1,5 @@
 Sentry.init do |config|
-  config.dsn = ENV.fetch('SENTRY_DNS') { '' }
+  config.dsn = ENV.fetch('SENTRY_DSN') { '' }
   config.environment = ENV.fetch('SENTRY_ENVIRONMENT') { 'production' }
   config.enabled_environments = %w[staging production]
   config.breadcrumbs_logger = [:active_support_logger, :http_logger]
@@ -7,13 +7,13 @@ Sentry.init do |config|
   # Set traces_sample_rate to 1.0 to capture 100%
   # of transactions for performance monitoring.
   # We recommend adjusting this value in production.
-  config.traces_sample_rate = 1.0
+  config.traces_sample_rate = 0
   # or
-  config.traces_sampler = lambda do |context|
-    true
-  end
+  # config.traces_sampler = lambda do |context|
+  #   true
+  # end
   # Set profiles_sample_rate to profile 100%
   # of sampled transactions.
   # We recommend adjusting this value in production.
-  config.profiles_sample_rate = 1.0
+  config.profiles_sample_rate = 0
 end
